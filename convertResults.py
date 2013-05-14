44ebc55c5b85f04ed5e316da218171aee063ac49
Caleb Robinson <dcrobins@oscar.(none)>
2013-05-14 13:35:35 -0500

    Argumentized scripts

--- convertResults.py
+++ convertResults.py
@@ -1,5 +1,14 @@
-f = open("submission3.csv","w")
-for i in open("results.txt","r"):
+#!/usr/bin/python
+import sys
+
+if len(sys.argv)!=3:
+    print "Usage: ./convertResults.py <input.txt> <output.csv>\n"
+    sys.exit(0)
+
+inFile = sys.argv[1]
+outFile = sys.argv[2]
+f = open(outFile,"w")
+for i in open(inFile,"r"):
     a = i.split("--")
     f.write(a[1].strip()+"\n")
 f.close()
--- lbpApproach.py
+++ lbpApproach.py
@@ -2,7 +2,7 @@
 import cv2
 import numpy as np
 import time
-import os
+import os, sys
 import re
 
 startTime = time.time()
@@ -24,7 +24,12 @@ DATA_DIR = "./data/"
 TRAINING_DIR = "./training/"
 IMAGES_DIR = "./images/"
 
+if len(sys.argv)!=2:
+    print "Usage: ./lbpApproach.py <outputName>\n"
+    sys.exit(0)
 
+outName = sys.argv[1]
+print outName+".txt"
 startTime = time.time()
 print "Setting up data for training"
 dirList=os.listdir(TRAINING_DIR)
@@ -35,12 +40,12 @@ for fname in dirList:
     images.append(image)
 print "Finished setting up images in %s seconds" % (time.time()-startTime)
 print "Starting training"
-model = cv2.createLBPHFaceRecognizer(radius=1,neighbors=8)
+model = cv2.createMinMaxLBPHFaceRecognizer(radius=1,neighbors=8)
 model.setInt("recordFaces",0)
 model.train(np.asarray(images),np.asarray(labels))
 print "Finished training model in %s seconds" % (time.time()-startTime)
 
-f=open("results.txt","w")
+f=open(outName+".txt","w")
 dirList=os.listdir(IMAGES_DIR)
 dirList.sort(key=natural_keys)
 i=1
